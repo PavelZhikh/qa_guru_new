814f60f0a265c21e7f08220929bda43a5635281e
Pavel Zhikharev <jiharpv@gmail.com>
2026-02-27 22:23:01 +0300

    Added asked changes

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -11,30 +11,28 @@ public class TestData {
     public String birthYear = String.format(String.valueOf(faker.number().numberBetween(1900,2100)));
     public String birthMonth = faker.options().option("January", "February", "March", "April",
             "May", "June", "July", "August", "September", "October", "November", "December");
-    public String day = String.format(String.valueOf(faker.number().numberBetween(10, 28)));
+    public String day = String.format("%02d", faker.number().numberBetween(1,28));
     public String subject = faker.options().option("Math", "Arts", "Accounting", "Social Studies");
     public String hobby = faker.options().option("Sports", "Reading", "Music");
     public String file = faker.options().option("real-ronaldo.jpg", "barselona-messi.jpg");
     public String address = faker.address().fullAddress();
-    public String state = faker.options().option("NCR", "Haryana", "Rajasthan");
+    public String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
     public String city = chooseCityBasedOnState();
+    public static String modalHeader = "Thanks for submitting the form";
 
     public String chooseCityBasedOnState() {
-        if (state.equals("NCR")) {
-            city = faker.options().option("Delhi", "Gurgaon", "Noida");
-        }
-        if (state.equals("Uttar Pradesh")) {
-            city = faker.options().option("Agra", "Lucknow", "Merrut");
-        }
-        if (state.equals("Haryana")) {
-            city = faker.options().option("Karnal", "Panipat");
-        }
-        if (state.equals("Rajasthan")) {
-            city = faker.options().option("Jaipur", "Jaiselmer");
-        }
+        String city = selectCity(state);
 
         return city;
-    }
+        }
 
-    public static String modalHeader = "Thanks for submitting the form";
+    public String selectCity(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> null;
+        };
+    }
 }
