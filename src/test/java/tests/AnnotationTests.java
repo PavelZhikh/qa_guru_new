410e75c759986202a78b56fc126461e3f840c86d
Pavel Zhikharev <jiharpv@gmail.com>
2026-03-09 20:46:23 +0300

    Added MethodSource

--- src/test/java/tests/AnnotationTests.java
+++ src/test/java/tests/AnnotationTests.java
@@ -3,20 +3,29 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 //import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 //import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AnnotationTests {
 
     @BeforeAll
     public static void setup() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
         Configuration.timeout = 20000;
     }
 
@@ -75,4 +84,23 @@ public class AnnotationTests {
         $(".navigation-search-results__anchor").shouldHave(text(expectedValue));
     }
 
+    static Stream<Arguments> sportsSiteShouldDisplayCorrectButtons() {
+        return Stream.of(
+                Arguments.of(
+                        List.of("Моя лента", "Ставки", "Футбол", "Фигурка", "Хоккей", "Баскет",
+                                "Теннис", "Бокс", "Авто", "Здоровье", "Медиафутбол", "Прожектор")
+                )
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Тестирование @MethodSource, проверка кнопок")
+    @Tag("Sports")
+    void sportsSiteShouldDisplayCorrectButtons(List<String> expectedButtons){
+        $$(".navigation-navbar__list li").filter(visible)
+                .shouldHave(texts(expectedButtons));
+    }
 }
+
+
+
