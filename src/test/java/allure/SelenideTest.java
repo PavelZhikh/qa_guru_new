44070dbb66d9caf98242fa40ea37f275a210ba94
Pavel Zhikharev <jiharpv@gmail.com>
2026-04-22 19:05:46 +0300

    Added SelenideTest

---
+++ src/test/java/allure/SelenideTest.java
@@ -0,0 +1,30 @@
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys("PavelZhikh/qa_guru_new");
+        $("#query-builder-test").submit();
+
+        $(linkText("PavelZhikh/qa_guru_new")).click();
+        $("#issues-tab").click();
+        $(withText("No results")).should(Condition.exist);
+    }
+}
