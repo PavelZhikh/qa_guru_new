a404bbb94ac972c7e7a49cba5d95fc648e917acb
PavelZhikh <jiharpv@gmail.com>
2026-05-01 13:31:17 +0300

    Added registration tests for Jenkins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("io.qameta.allure") version "2.11.2"
+    id("io.qameta.allure") version "2.12.0"
 }
 
 group = "com.jiharpv"
@@ -8,13 +8,13 @@ version = "1.0-SNAPSHOT"
 
 allure {
     report {
-        version.set("2.17.3")
+        version.set("2.32.0")
     }
     adapter {
         aspectjWeaver.set(true)
         frameworks {
             junit5 {
-                adapterVersion.set("2.20.0")
+                adapterVersion.set("2.32.0")
             }
         }
     }
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     dependencies {
-        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation(platform("org.junit:junit-bom:5.14.1"))
         testImplementation("org.junit.jupiter:junit-jupiter")
         testImplementation("com.codeborne:selenide:7.14.0")
 //        testImplementation("com.codeborne:selenide:7.0.1")
@@ -39,12 +39,33 @@ dependencies {
         testImplementation("io.rest-assured:rest-assured:6.0.0")
         testImplementation("io.rest-assured:json-schema-validator:6.0.0")
         testImplementation("io.qameta.allure:allure-selenide:2.19.0")
+        testImplementation("org.slf4j:slf4j-simple:2.0.17")
     }
 }
 
-tasks.test {
+/*tasks.test {
     useJUnitPlatform()
 
+}*/
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        events("started", "skipped", "failed", "standard_error", "standard_out")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.SHORT
+    }
 }
+
+/*tasks.withType(Test) {
+    useJUnitPlatform()
+
+    testLogging {
+        lifecycle {
+            events "started", "skipped", "failed", "standard_error", "standard_out"
+            exceptionFormat "short"
+        }
+    }
+}*/
 
 
---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,54 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript(
+                "if(document.getElementById('fixedban')) document.getElementById('fixedban').remove();" +
+                        "if(document.getElementById('footer')) document.getElementById('footer').remove();"
+        );
+
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Petrov");
+        $("#userEmail").setValue("ivan@petrov.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("5454411545");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("2006");
+        $(".react-datepicker__day--023:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("jenkins_ui_img/1.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Ivan"), text("Petrov"),
+                text("ivan@petrov.com"), text("5454411545"));
+    }
+}
